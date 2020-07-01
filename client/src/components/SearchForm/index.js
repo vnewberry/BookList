6bd8ec01599d3010dfcccde5769f7aa11e0de9da
Vance Newberry <vancenewberry@gmail.com>
2020-06-30 19:15:44 -0500

    passing props to searchform

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
 
-function SearchForm() {
+function SearchForm(props) {
   return (
     <div className="container h-100">
     <div className="d-flex justify-content-center h-100">
       <div className="searchbar">
-        <input className="search_input" type="text" name="" placeholder="Search..."/>
-        <a href="#" className="search_icon"><i class="fas fa-search"></i></a>
+        <input className="search_input" type="text" name="" placeholder="Search..." {...props}/>
+        <a href="#" className="search_icon">{props.children}<i class="fas fa-search"></i></a>
       </div>
     </div>
   </div>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,48 @@
 import React, { Component } from "react";
 import SearchForm from "../components/SearchForm";
-// import API from "../utils/API";
+import API from "../utils/API";
 
 
 class Search extends Component {
+  state = {
+    books: [],
+    title: "",
+    authors: "",
+    description: "",
+    image: "",
+    link: "",
+  };
+  
+  componentDidMount() {
+
+  }
+  
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  
+  handleFormSubmit = event => {
+
+    event.preventDefault();
+    API.searchBooks(this.state.title)
+      .then(res => {
+        this.setState({books: res.data.items});
+        // console.log(
+        //   res.data.items[0].volumeInfo.title, 
+        //   res.data.items[0].volumeInfo.authors, 
+        //   res.data.items[0].volumeInfo.description,
+        //   res.data.items[0].volumeInfo.imageLinks.thumbnail,
+        //   res.data.items[0].volumeInfo.previewLink,
+        //   res.data.items[0].volumeInfo.infoLink);
+          
+      } )
+      .catch(err => console.log(err))
+
+
+  };
   render() {
     return (
      <SearchForm/>
